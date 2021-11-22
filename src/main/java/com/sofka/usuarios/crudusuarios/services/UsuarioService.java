7071580842b3e8a735b31f698e98aa94559cad39
Antonio Alzate <antonioalzate7@gmail.com>
2021-11-22 16:09:49 -0500

    Creación de la clase UsuarioService donde se hace uso del repositorio para realizar acciones de listar y crear un nuevo usuario.

---
+++ src/main/java/com/sofka/usuarios/crudusuarios/services/UsuarioService.java
@@ -0,0 +1,23 @@
+package com.sofka.usuarios.crudusuarios.services;
+
+import com.sofka.usuarios.crudusuarios.models.UsuarioModel;
+import com.sofka.usuarios.crudusuarios.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return (ArrayList<UsuarioModel>) usuarioRepository.findAll();
+    }
+
+    public UsuarioModel guardarUsuario(UsuarioModel usuario){
+        return usuarioRepository.save(usuario);
+    }
+}
