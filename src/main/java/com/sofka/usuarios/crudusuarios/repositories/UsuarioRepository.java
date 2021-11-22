3471de0802640dae145930912d287264760cc1f8
Antonio Alzate <antonioalzate7@gmail.com>
2021-11-22 16:06:27 -0500

    Creación y configuración del archivo UsuarioRepository
    Este es el que va a interactuar con la base de datos.

---
+++ src/main/java/com/sofka/usuarios/crudusuarios/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.sofka.usuarios.crudusuarios.repositories;
+
+import com.sofka.usuarios.crudusuarios.models.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
+
+}
