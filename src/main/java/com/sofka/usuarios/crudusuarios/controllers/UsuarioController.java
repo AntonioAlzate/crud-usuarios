b7bea73670b3425b15ce0dbaf13d98d45425e9b8
Antonio Alzate <antonioalzate7@gmail.com>
2021-11-22 16:36:31 -0500

    Creación e implementación de los servicios de obtenerPorId, por prioridad y eliminar un usuario.

--- src/main/java/com/sofka/usuarios/crudusuarios/controllers/UsuarioController.java
+++ src/main/java/com/sofka/usuarios/crudusuarios/controllers/UsuarioController.java
@@ -5,7 +5,9 @@ import com.sofka.usuarios.crudusuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -22,5 +24,24 @@ public class UsuarioController {
     @PostMapping()
     public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
         return usuarioService.guardarUsuario(usuario);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id){
+        return usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return usuarioService.obtenerPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = usuarioService.eliminarUsuario(id);
+        if(ok)
+            return "Se elimino el usuario con id " + id;
+        else
+            return "No pudo ser eliminado el usuario con id " + id;
     }
 }
--- src/main/java/com/sofka/usuarios/crudusuarios/services/UsuarioService.java
+++ src/main/java/com/sofka/usuarios/crudusuarios/services/UsuarioService.java
@@ -5,7 +5,9 @@ import com.sofka.usuarios.crudusuarios.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -19,5 +21,22 @@ public class UsuarioService {
 
     public UsuarioModel guardarUsuario(UsuarioModel usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    public Optional<UsuarioModel> obtenerPorId(Long id){
+        return usuarioRepository.findById(id);
+    }
+
+    public ArrayList<UsuarioModel> obtenerPorPrioridad(Integer prioridad){
+        return usuarioRepository.findByPrioridad(prioridad);
+    }
+
+    public boolean eliminarUsuario(Long id){
+        try {
+            usuarioRepository.deleteById(id);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
     }
 }
